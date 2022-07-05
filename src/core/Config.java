8454d019b22ec451b05484e9c4b706093f7becf5
tnebes <tomislav.nebes@gmail.com>
2022-07-05 13:21:45 +0200

    javafx new version begin

--- src/core/Config.java
+++
@@ -1,9 +0,0 @@
-package core;
-
-public class Config {
-    public static final int WINDOW_X = 800;
-    public static final int WINDOW_Y = 600;
-    public static final String APP_NAME = "Java Weblog Parser";
-    public static final String APP_AUTHOR = "tnebes";
-    public static final String YEAR = String.valueOf(java.time.LocalDate.now().getYear());
-}
